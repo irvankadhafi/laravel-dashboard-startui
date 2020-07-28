cace37e2d904ac3bdf62d30e819c206bfab4441a
Irvan Kadhafi <irvan.kadhafi.tif18@polban.ac.id>
2020-07-28 15:10:52 +0700

    menambah middleware ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('/dashboard')->group(function () {
+Route::prefix('/dashboard')->middleware('auth')->group(function () {
     Route::view('home','dashboard.home');
     Route::view('test','dashboard.testhome');
 });
